6f9c6005a6f956e956f74589adaab05ecaf3d6d3
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-09 22:39:31 +0400

    Use ssh-private-keys cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name 'themis-finals-checker-result-py-lib'
 description 'Installs and configures themis.finals.checker.result package'
-version '1.0.0'
+version '1.1.0'
 
 recipe 'themis-finals-checker-app-py-lib', 'Installs and configures themis.finals.checker.result package'
 depends 'latest-git', '~> 1.1.11'
 depends 'git2', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 1.0.0'
